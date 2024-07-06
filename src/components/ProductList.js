043b75f707a160d7a78e96189323c4cbea198ad2
ErlandasL <erlandaslisauskas@gmail.com>
2024-07-06 22:10:10 +0300

    Add: pagination

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,6 +4,8 @@ import { fetchProducts } from '../services/api';
 
 function ProductList() {
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 8;
 
   useEffect(() => {
     const getProducts = async () => {
@@ -14,12 +16,20 @@ function ProductList() {
     getProducts();
   }, []);
 
+  // Calculate current products to display based on currentPage
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
     <div className="container">
-      <h1 className="text-center mt-3 mb-4">Products</h1>
+      <h1 className="text-center mt-4 mb-4">Products</h1>
       <div className="row row-cols-1 row-cols-md-4 g-4">
-        {products.map(product => (
-          <div key={product.id} className="col">
+        {currentProducts.map(product => (
+          <div key={product.id} className="col mb-4">
             <div className="card h-100">
               <img src={product.image} className="card-img-top" alt={product.title} />
               <div className="card-body">
@@ -31,6 +41,17 @@ function ProductList() {
             </div>
           </div>
         ))}
+      </div>
+      <div className="d-flex justify-content-center mt-4">
+        <nav aria-label="Page navigation example">
+          <ul className="pagination">
+            {Array.from({ length: Math.ceil(products.length / productsPerPage) }, (_, index) => (
+              <li key={index} className={`page-item ${index + 1 === currentPage ? 'active' : ''}`}>
+                <button onClick={() => paginate(index + 1)} className="page-link">{index + 1}</button>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </div>
   );
